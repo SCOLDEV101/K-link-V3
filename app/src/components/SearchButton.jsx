5d4fc5a350e546e131665b0f38a614ea20cb25d0
pawimai <pulooluuu@gmail.com>
2024-11-08 17:40:30 +0700

    PK: submit

--- app/src/components/SearchButton.jsx
+++ app/src/components/SearchButton.jsx
@@ -13,7 +13,7 @@ function SearchButton({ fromFeature }) {
           borderRadius: "26px",
           backgroundColor: "#E7E7E7",
         }}>Search...</div>
-          <div className="px-3 py-1" style={{border:"10px solid #ffffff" , borderRadius:'26px',position:"absolute", top:"10%",right:"0",background: "linear-gradient(to right, #F96E20 2%, #FBBF66 49%, #F89603 100%)"
+          <div className="px-3 py-1" style={{border:"10px solid #ffffff" , borderRadius:'26px',position:"absolute", top:"10%",right:"0",background: "linear-gradient(-201deg,  #FBBF66 20%, #F89603 80%)"
 }}><FaSearch className="text-white" style={{marginBottom:"0.1rem"}}/></div>
       </Link>
     </div>
