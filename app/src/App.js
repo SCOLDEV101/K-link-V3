47cb3d19fa3b4f71a2a863db9fdccdd02e185dd7
Kasidit Thong-on <141108371+kasidit-th@users.noreply.github.com>
2025-03-01 07:40:32 +0700

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo from './K-Link-Logo.svg'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="K-LINK Logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          K-LINK is a platform that enables students to connect, learn, and collaborate through various activities.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://k-link-v3.vercel.app" 
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          K-LINK
         </a>
       </header>
     </div>
