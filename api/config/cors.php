9728d9d873d30f10eab1f5fcde865a1834c9d6fa
Volthnuts <jakgree.wnts@gmail.com>
2025-04-06 22:49:04 +0700

    แก้ cors

--- api/config/cors.php
+++ api/config/cors.php
@@ -2,26 +2,11 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://k-link-v3.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
@@ -34,3 +19,4 @@ return [
     'supports_credentials' => true,
 
 ];
+
